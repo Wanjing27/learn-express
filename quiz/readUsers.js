0575200a0478044dabb98d975807ad3c6c229054
wanjing27 <wanjing0227@gmail.com>
2024-03-13 15:16:09 -0400

    error correct

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,21 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+
+router.get('/username/:name', (req, res) => {
+  const username = req.params.name;
+  const user = req.users.find(user => user.username === username);
+  if (user) {
+    res.json({ email: user.email });
+  } else {
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
+router.get('/usernames', (req, res) => {
+  let usernames = req.users.map(function(user) {
+    return {id: user.id, username: user.username};
+  });
+  res.send(usernames);
+});
+
+module.exports = router;
--- quiz/server.js
+++ quiz/server.js
@@ -5,62 +5,62 @@ const app = express();
 var cors = require('cors');
 const port = 8000;
 
-let users;
-fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
-  console.log('reading file ... ');
-  if(err) throw err;
-  users = JSON.parse(data);
-})
+// Middleware to handle CORS
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-const addMsgToRequest = function (req, res, next) {
-  if(users) {
-    req.users = users;
-    next();
-  }
-  else {
-    return res.json({
-        error: {message: 'users not found', status: 404}
-    });
-  }
-}
-
-app.use(cors({origin: 'http://localhost:3000'}));
+// Middleware to parse JSON and URL-encoded bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Read users data from file
+let users;
+fs.readFile(path.resolve(__dirname, '../data/users.json'), function (err, data) {
+  console.log('Reading file ...');
+  if (err) throw err;
+  users = JSON.parse(data);
+
+  // Middleware to add users data to the request object
+  app.use(addMsgToRequest);
+
+  // Route to get usernames
+  app.get('/read/usernames', (req, res) => {
+    if (!users) {
+      return res.status(404).json({ error: 'Users not found' });
+    }
+    let usernames = users.map(function (user) {
+      return { id: user.id, username: user.username };
+    });
+    res.send(usernames);
+  });
+
+  // Route to add a new user
+  app.post('/write/adduser', (req, res) => {
+    if (!users) {
+      return res.status(404).json({ error: 'Users not found' });
+    }
+    let newuser = req.body;
+    users.push(newuser);
+    fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(users), (err) => {
+      if (err) console.log('Failed to write');
+      else console.log('User Saved');
+    });
+    res.send('done');
+  });
+
+  // Start the server after reading the file
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+});
+
 // Middleware to add users data to the request object
-app.use(addMsgToRequest);
-
-// Route to search for users by username and see their email
-app.get('/read/username/:name', (req, res) => {
-  const username = req.params.name;
-  const user = req.users.find(user => user.username === username);
-  if (user) {
-    res.json({ email: user.email });
+const addMsgToRequest = function (req, res, next) {
+  if (users) {
+    req.users = users;
+    next();
   } else {
-    res.status(404).json({ error: 'User not found' });
+    return res.status(404).json({
+      error: { message: 'Users not found', status: 404 }
+    });
   }
-});
-
-// Route to get usernames
-app.get('/read/usernames', (req, res) => {
-  let usernames = req.users.map(function(user) {
-    return {id: user.id, username: user.username};
-  });
-  res.send(usernames);
-});
-
-// Route to add a new user
-app.post('/write/adduser', (req, res) => {
-  let newuser = req.body;
-  req.users.push(newuser);
-  fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users), (err) => {
-    if (err) console.log('Failed to write');
-    else console.log('User Saved');
-  });
-  res.send('done');
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+};
